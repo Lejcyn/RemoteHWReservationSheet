5e11098101bb12eeb1517f4384180e81c0867dd3
Lejcyn <kowalczyk.tomasz00@gmail.com>
2021-10-04 23:42:17 +0200

    Managed to read arp
    Managed to read arp code

--- NetworkHWList.py
+++ NetworkHWList.py
@@ -1,4 +1,8 @@
-import os
+import os,re
+f= os.popen('arp -a') 
+data = f.read()
 
-
-print(os.system("arp -a"))
+for line in re.findall('([-.0-9]+)\s+([-0-9a-f]{17})\s+(\w+)',data):
+  print(line)
+  if line[1] == "01-00-5e-00-00-16":
+    print(line[0]+"IP found")
