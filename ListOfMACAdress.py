25fcb2e423b2916a119eb2f594b89bf797bfe357
Lejcyn <kowalczyk.tomasz00@gmail.com>
2021-10-05 13:21:06 +0200

    Writing to file
    Added writing to the file

--- ListOfMACAdress.py
+++ ListOfMACAdress.py
@@ -1,10 +1,12 @@
-Devices =["HUBUDPXIE8880","01-00-5e-00-00-16"],["cRIO9037TSE","00:80:2F:25:FC:57"],["cRIO9037LIB","00:80:2F:23:9B:8B"]
+from os import name
+
+Devices =["HUBUDPXIE8880 TOP","00-80-2F-19-D9-56"],["cRIO9037TSE","00:80:2F:25:FC:57"],["cRIO9037LIB","00:80:2F:23:9B:8B"],["NI-PXI Bottom","00-80-2F-12-DF-2B"]
 class Device:
    
-    def __init__(Name,MAC):
+    def __init__(self,name,mac):
         self.IPadress="NO IP"
-        self.Name=""
-        self.MAC=""
+        self.Name=name
+        self.MAC=mac
 
     def getIP(self):
         return self.IPadress
@@ -21,11 +23,12 @@ class Device:
 def InitDevices ():
     dlist=[]
     for device in Devices:
-        dlist.append(Device(device[0]),device[1])
+        dlist.append(Device(device[0],device[1]))
     return(dlist)
 
+#ConstructorTest
+# Test=InitDevices()
 
-Test=InitDevices()
+# for dev in Test:
+#     dev.printout()
 
-for dev in Test:
-    dev.printout()
--- NetworkHWList.py
+++ NetworkHWList.py
@@ -1,5 +1,8 @@
 import os,re
 from ListOfMACAdress import *
+open('IPList.txt', 'w').close()
+Tfile = open("IPList.txt", "r+")
+Tfile.truncate(0) # need '0' when using r+
 f= os.popen('arp -a') 
 data = f.read()
 #Name= [IPadress,MAC]
@@ -7,6 +10,11 @@ ListOfDevices=InitDevices()
 for line in re.findall('([-.0-9]+)\s+([-0-9a-f]{17})\s+(\w+)',data):
   for device in ListOfDevices:
     if line[1] == device.getMAC():
+      device.IPadress=line[0]
 
-
-print("done")
+for dev in ListOfDevices:
+    dev.printout()
+    Tfile.writelines(dev.Name+dev.MAC+dev.IPadress)
+   
+Tfile.close()
+print("Done")
