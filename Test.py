8365032a350a2422f4d95e267360908604ef5328
Lejcyn <kowalczyk.tomasz00@gmail.com>
2021-11-10 15:20:41 +0100

    First working version with Ping

--- Test.py
+++ Test.py
@@ -1,8 +1,13 @@
 # import os
 import re
 import subprocess
+import os
 # f= os.popen('arp -a') 
-# for kek in range(2,50):
-    #print(f"ping 10.92.6.{kek} -w 1000 -n 1")
-    #res=os.popen("f"ping 10.92.6.{kek} -w 1000 -n 1"")
+# for ip in range(2,50):
+#     res=os.popen(f"ping 10.92.1.{ip} -w 1000 -n 1")# For office
+#     #res=os.popen(f"ping 10.92.6.{ip} -w 1000 -n 1")#For AE Lib
+# Devices =["PXIe Bottom","00:80:2F:12:DF:2C"],["HUBUDPXIE8880 TOP1","00-80-2F-19-D9-56"],["HUBUDPXIE8880 TOP2","00:80:2F:19:D9:57"],["NI-cRIO-9054-0","00:80:2F:22:D3:3A"],["NI-cRIO-9054-1","00:80:2F:22:D3:3B"],["NI-sbRIO-9607-0","00:80:2F:16:F3:4E"],["NI-sbRIO-9607-1","00:80:2F:16:F3:4F"],["cRIO9037TSE-0","00:80:2F:25:FC:56"],["cRIO9037TSE-1","00:80:2F:25:FC:57"],["cRIO9037LIB","00:80:2F:23:9B:8B"],["NI-PXI Bottom","00-80-2F-12-DF-2B"],["NI-cRIO-9036-PLU-0","00:80:2F:21:43:AE"],["NI-cRIO-9036-PLU-1","00:80:2F:21:43:AF"]
+# for idx,val in enumerate(Devices):
+#     Devices[idx][1]=val[1].replace(":","-") 
 
+# print(Devices)
